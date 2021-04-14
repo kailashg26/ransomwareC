b8fbf2e69f83403408999c24d4dcfa9d85917fd1
KvN0X00 <65466003+Kanui-C@users.noreply.github.com>
2021-04-14 11:06:46 -0300

    Update ransomware.c

--- ransomware.c
+++ ransomware.c
@@ -1,23 +1,8 @@
-#include <stdio.h>  // Entrada e saida padrao de dados
-#include <stdlib.h> // Biblioteca padrao
-#include <string.h> // Tratar strings
-#include <dirent.h> // Entrada de diretorios
-#include <errno.h> 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-
+#include <stdio.h>
 #include "libs/funcs.h"
 
-//Funçao principal
+
 int main(int argc, char *argv[]){
-	int status;
 	if(argc != 2 ){
 		fprintf(stderr, "Use: %s [DIRETORIO]", argv[0]);
 		return 1;
