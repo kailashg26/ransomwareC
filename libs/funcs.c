e1e1da977826e96862d635f760567c82fd153d90
KvN0X00 <65466003+Kanui-C@users.noreply.github.com>
2021-04-20 16:55:14 -0300

    Update funcs.c

--- libs/funcs.c
+++ libs/funcs.c
@@ -6,12 +6,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <errno.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 
 #include "funcs.h"
 
